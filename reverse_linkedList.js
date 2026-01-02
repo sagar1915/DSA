968e04812b2a772dd919cd43507acb9b32a84e12
sagarch <sagar192010@gmail.com>
2026-01-02 19:55:07 +0530

    reverse linkedlist

---
+++ reverse_linkedList.js
@@ -0,0 +1,13 @@
+var reverseList = function (head) {
+	let prev = null;
+	let curr = head;
+
+	while (curr) {
+		let temp = curr.next;
+		curr.next = prev;
+		prev = curr;
+		curr = temp;
+	}
+	head = prev;
+	return head;
+};
